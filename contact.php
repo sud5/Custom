af5e5ecc1927af02ff3fb2f31224f031ec10b25c
Yogendra05-Singh <yogisingh0505@gmail.com>
2025-03-16 16:49:27 +0000

    Updated PHP files and added a new image

--- contact.php
+++ contact.php
@@ -2,6 +2,14 @@
 <html lang="en">
 
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KWMZVMMF');</script>
+         <!-- End Google Tag Manager -->
+
         <title>2PI Solutions</title>
         <link rel="icon" href="images/favicon.ico" type="image/gif" sizes="16x16">
         <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
@@ -50,6 +58,11 @@
     </head>
 
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWMZVMMF"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         <div id="wrapper">
             <!-- page preloader begin -->
             <div id="de-loader"></div>
